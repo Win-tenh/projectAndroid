a00cbb9d407bb80de9fa6b8deb550724a44b7fcd
W-Thang <nqtfacebookanhxtanh@gmail.com>
2024-05-21 12:14:36 +0700

    update 2

---
+++ B15StudentManagement/app/src/main/java/com/example/b15studentmanagement/DatabaseHelper.java
@@ -0,0 +1,87 @@
+package com.example.b15studentmanagement;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "students.db";
+    private static final int DATABASE_VERSION = 2; //
+    public static final String TABLE_STUDENTS = "students";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_SIZE = "size";
+    private SQLiteDatabase db;
+    private ArrayList<String> students;
+
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_STUDENTS + " (" +
+                    COLUMN_ID + " TEXT PRIMARY KEY, " +
+                    COLUMN_NAME + " TEXT, " +
+                    COLUMN_SIZE + " TEXT " +
+                    ");";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_STUDENTS);
+        onCreate(db);
+    }
+
+    // lấy danh sách sinh viên và trả về ArrayList<String>
+    public ArrayList<String> getAllStudents() {
+        students = new ArrayList<>();
+
+        db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_STUDENTS, null);
+        while (cursor.moveToNext()) {
+            String id = cursor.getString(0);
+            String name = cursor.getString(1);
+            String size = cursor.getString(2);
+            students.add(id + " - " + name + " - " + size);
+        }
+        cursor.close();
+        db.close();
+        return students;
+    }
+
+    // kiểm tra xem sinh viên có tồn tại trong cơ sở dữ liệu hay không
+    public boolean checkStudent(String id) {
+        db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_STUDENTS + " WHERE " + COLUMN_ID + " = '" + id + "'", null);
+        boolean exists = cursor.getCount() > 0;
+        cursor.close();
+        db.close();
+        return exists;
+    }
+
+    // thêm sinh viên vào cơ sở dữ liệu
+    public void addStudent(String id, String name, String size) {
+        db = getWritableDatabase();
+        db.execSQL("INSERT INTO " + TABLE_STUDENTS + " VALUES ('" + id + "', '" + name + "', '" + size + "');");
+        db.close();
+    }
+
+    // cập nhật thông tin sinh viên
+    public void updateStudent(String id, String name, String size) {
+        db = getWritableDatabase();
+        db.execSQL("UPDATE " + TABLE_STUDENTS + " SET " + COLUMN_NAME + " = '" + name + "', " + COLUMN_SIZE + " = '" + size + "' WHERE " + COLUMN_ID + " = '" + id + "';");
+        db.close();
+    }
+
+    // xóa sinh viên khỏi cơ sở dữ liệu
+    public void deleteStudent(String id) {
+        db = getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_STUDENTS + " WHERE " + COLUMN_ID + " = '" + id + "';");
+    }
+}
--- B15StudentManagement/app/src/main/java/com/example/b15studentmanagement/MainActivity.java
+++ B15StudentManagement/app/src/main/java/com/example/b15studentmanagement/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.b15studentmanagement;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edt_id, edt_name, edt_size;
+    private Button btn_add, btn_update, btn_delete;
+    private ListView lv_class;
+    private DatabaseHelper db;
+    private ArrayAdapter<String> studentAdapter;
+    private ArrayList<String> studentList;
+    private int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,119 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // ánh xạ các view trong layout
+        edt_id = findViewById(R.id.edt_id);
+        edt_name = findViewById(R.id.edt_name);
+        edt_size = findViewById(R.id.edt_size);
+        btn_add = findViewById(R.id.btn_add);
+        btn_update = findViewById(R.id.btn_update);
+        btn_delete = findViewById(R.id.btn_delete);
+        lv_class = findViewById(R.id.lv_class);
+
+        // không focus vào edit text nào, khi nào nhấn vào mới được sửa
+
+        // truyền dữ liệu từ db lên listView
+        db = new DatabaseHelper(this);
+        studentList = db.getAllStudents();
+        studentAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, studentList);
+        lv_class.setAdapter(studentAdapter);
+
+        // xử lý sự kiện
+
+        // nút thêm student
+        btn_add.setOnClickListener(v -> {
+            String id = edt_id.getText().toString();
+            String name = edt_name.getText().toString();
+            String size = edt_size.getText().toString();
+            if (validate(id, name, size)) {
+                // kiểm tra mã lớp
+                if (db.checkStudent(id)) {
+                    Toast.makeText(this, "Mã lớp đã tồn tại!", Toast.LENGTH_SHORT).show();
+                    edt_id.requestFocus();
+                    return;
+                }
+                // thêm student
+                db.addStudent(id, name, size);
+                Toast.makeText(this, "Thêm lớp thành công!", Toast.LENGTH_SHORT).show();
+                studentList.add(id + " - " + name + " - " + size);
+                studentAdapter.notifyDataSetChanged();
+
+                // reset edit text
+                reset();
+            }
+        });
+
+        // chọn vào 1 dòng listView
+        lv_class.setOnItemClickListener((parent, view, position, id) -> {
+            this.position = position;
+            String[] student = studentList.get(position).split(" - ");
+            edt_id.setText(student[0]);
+            edt_name.setText(student[1]);
+            edt_size.setText(student[2]);
+            edt_id.setEnabled(false);
+        });
+
+        // nút sửa student
+        btn_update.setOnClickListener(v -> {
+            String id = edt_id.getText().toString();
+            String name = edt_name.getText().toString();
+            String size = edt_size.getText().toString();
+            if (validate(id, name, size)) {
+                // kiểm tra mã lớp
+                if (!db.checkStudent(id)) {
+                    Toast.makeText(this, "Mã lớp không tồn tại!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // sửa student
+                db.updateStudent(id, name, size);
+                Toast.makeText(this, "Sửa lớp thành công!", Toast.LENGTH_SHORT).show();
+                studentList.set(position, id + " - " + name + " - " + size);
+                studentAdapter.notifyDataSetChanged();
+
+                // reset edit text
+                reset();
+            }
+        });
+
+        // nút xóa student
+        btn_delete.setOnClickListener(v -> {
+            String id = edt_id.getText().toString();
+            if (id.isEmpty()) {
+                Toast.makeText(this, "Hãy chọn lớp cần xóa!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!db.checkStudent(id)) {
+                Toast.makeText(this, "Mã lớp không tồn tại!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            // xóa student
+            db.deleteStudent(id);
+            Toast.makeText(this, "Xóa lớp thành công!", Toast.LENGTH_SHORT).show();
+            studentList.remove(position);
+            studentAdapter.notifyDataSetChanged();
+            reset();
+        });
+    }
+
+    // validates: mã lớp không được trùng, sĩ số > 0, các trường không được để trống
+    public boolean validate(String id, String name, String size) {
+        if (id.isEmpty() || name.isEmpty() || size.isEmpty()) {
+            Toast.makeText(this, "Hãy nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (Integer.parseInt(size) <= 0) {
+            Toast.makeText(this, "Sĩ số phải lớn hơn 0!", Toast.LENGTH_SHORT).show();
+            edt_size.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void reset() {
+        edt_id.setText("");
+        edt_name.setText("");
+        edt_size.setText("");
     }
 }
