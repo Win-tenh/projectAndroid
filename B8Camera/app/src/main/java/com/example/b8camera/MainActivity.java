ee5dd158102818dfb9de18eace63313723ebc992
W-Thang <nqtfacebookanhxtanh@gmail.com>
2024-05-22 23:14:15 +0700

    Hoàn thiện permissions

--- B8Camera/app/src/main/java/com/example/b8camera/MainActivity.java
+++ B8Camera/app/src/main/java/com/example/b8camera/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.b8camera;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
@@ -15,6 +17,7 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
@@ -26,18 +29,8 @@ import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imgView;
-    private ImageButton btn_capture;
-    private ActivityResultLauncher cameraLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult o) {
-                    if (o.getResultCode() == RESULT_OK) {
-                        Bitmap photo = (Bitmap) o.getData().getExtras().get("data");
-                        imgView.setImageBitmap(photo);
-                    }
-                }
-            });
+    private ImageButton btn_capture, btn_gallery;
+    private String[] permissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,24 +45,75 @@ public class MainActivity extends AppCompatActivity {
 
         imgView = findViewById(R.id.imgView);
         btn_capture = findViewById(R.id.btn_capture);
+        btn_gallery = findViewById(R.id.btn_gallery);
+
+        permissions = new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
 
         btn_capture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent cameraIntent = new Intent(ACTION_IMAGE_CAPTURE);
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) !=
-                        PackageManager.PERMISSION_GRANTED)
-                {
-                    ActivityCompat.requestPermissions(
-                            MainActivity.this,
-                            new String[]{Manifest.permission.CAMERA}, 1);
-                    return;
+                if (!hasPermissions(MainActivity.this, permissions)) {
+                    ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+                } else {
+                    cameraLauncher.launch(cameraIntent);
                 }
-                cameraLauncher.launch(cameraIntent);
+
             }
         });
 
+        // xử lý sự kiện click cho btn_gallery: mở thư viện chứa ảnh
+        btn_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
+
+    private boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Camera Permission is granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Camera Permission is denied", Toast.LENGTH_SHORT).show();
+            }
+
+            if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Use Storage Permission is granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Use Storage Permission is denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private ActivityResultLauncher cameraLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult o) {
+                    if (o.getResultCode() == RESULT_OK) {
+                        Bitmap photo = (Bitmap) o.getData().getExtras().get("data");
+                        imgView.setImageBitmap(photo);
+                    }
+                }
+            });
 
 }
